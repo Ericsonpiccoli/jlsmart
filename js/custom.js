c473fbfea1cbd481869c22dedf4f3bb2a33f7f2e
Ericson piccoli <167118982+Ericsonpiccoli@users.noreply.github.com>
2024-09-30 21:39:42 -0300

    Add files via upload

--- js/custom.js
+++ js/custom.js
@@ -71,3 +71,38 @@
 
 
 })()
+ // Habilita ou desabilita o campo de quantidade com base na seleção do produto
+document.getElementById("produtos").addEventListener("change", function() {
+    var quantidadeInput = document.getElementById("quantidadeProduto");
+    if (this.value) {
+        quantidadeInput.disabled = false; // habilita o campo de quantidade quando um produto é selecionado
+    } else {
+        quantidadeInput.disabled = true; // desabilita se não há seleção
+    }
+});
+
+function enviarWhatsApp() {
+    var nome = document.getElementById("nome").value;
+    var sobrenome = document.getElementById("sobrenome").value;
+    var email = document.getElementById("email").value;
+    var endereco = document.getElementById("endereco").value;
+    var produto = document.getElementById("produtos").value;
+    var quantidade = document.getElementById("quantidadeProduto").value;
+    var mensagem = document.getElementById("mensagem").value;
+
+    // Validação dos campos obrigatórios
+    if (!nome || !sobrenome || !email || !endereco || !produto || !quantidade) {
+        alert('Por favor, preencha todos os campos obrigatórios.');
+        return;
+    }
+
+    // Monta a mensagem para o WhatsApp
+    var whatsappMessage = `Nome: ${nome}\nSobrenome: ${sobrenome}\nTelefone: +55 54 99685-3250\nEmail: ${email}\nEndereço: ${endereco}\nProduto: ${produto}\nQuantidade: ${quantidade}\nMensagem: ${mensagem}`;
+    
+    // URL do WhatsApp com a mensagem formatada
+    var whatsappURL = `https://api.whatsapp.com/send?text=${encodeURIComponent(whatsappMessage)}`;
+    
+    // Abre a URL do WhatsApp em uma nova aba
+    window.open(whatsappURL, '_blank');
+}
+
